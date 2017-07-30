a45028012e316fdb3ca6d551a4b9f7cf754b4423
Jarred Allen <jarredallen73@gmail.com>
2017-07-30 07:10:45 -0700

    Finished GUI's functional bits
    Now I need to store the data in a Google Sheets

--- app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/DataActivity.java
+++ app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/DataActivity.java
@@ -3,8 +3,10 @@ package com.github.troyhighschoolband.band_uniform_tracking_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -13,36 +15,79 @@ import java.util.Arrays;
 
 public class DataActivity extends AppCompatActivity {
 
+    private EditText barcodeNumber;
+    private EditText name;
+    private EditText itemType;
+    private EditText itemSize;
+    private EditText itemNumber;
+
+    private Button backButton;
+    private Button submitButton;
+
+    private TextView errorLog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
 
-        Intent intent = getIntent();
-        long bn = intent.getLongExtra(ScanActivity.barcodeNumberName, -1);
+        try {
+            Intent intent = getIntent();
+            long bn = intent.getLongExtra(ScanActivity.barcodeNumberName, -1);
 
-        TextView barcodeNumber = (TextView) findViewById(R.id.BarcodeNumberInput);
-        TextView name = (TextView) findViewById(R.id.ItemUserInput);
-        TextView itemType = (TextView) findViewById(R.id.ItemTypeInput);
-        TextView itemSize = (TextView) findViewById(R.id.ItemSizeInput);
-        TextView itemNumber = (TextView) findViewById(R.id.ItemNumberInput);
-        Button backButton = (Button) findViewById(R.id.backButton);
-        Button submitButton = (Button) findViewById(R.id.submitButton);
+            barcodeNumber = (EditText) findViewById(R.id.BarcodeNumberInput);
+            name = (EditText) findViewById(R.id.ItemUserInput);
+            itemType = (EditText) findViewById(R.id.ItemTypeInput);
+            itemSize = (EditText) findViewById(R.id.ItemSizeInput);
+            itemNumber = (EditText) findViewById(R.id.ItemNumberInput);
+            backButton = (Button) findViewById(R.id.backButton);
+            submitButton = (Button) findViewById(R.id.submitButton);
+            errorLog = (TextView) findViewById(R.id.errorLog);
 
-        barcodeNumber.setText(String.valueOf(bn));
-        Data loaded = loadData(bn);
-        if (loaded.worked) {
-            name.setText(loaded.name);
-            itemType.setText(loaded.itemType);
-            itemSize.setText(loaded.itemSize);
-            itemNumber.setText(loaded.itemNumber);
+            if (bn != -1) {
+                barcodeNumber.setText(String.valueOf(bn));
+            }
+
+            Data loaded = loadData(bn);
+            if (loaded.worked) {
+                name.setText(loaded.name);
+                itemType.setText(loaded.itemType);
+                itemSize.setText(loaded.itemSize);
+                itemNumber.setText(loaded.itemNumber);
+            }
+
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        storeData(Long.parseLong(barcodeNumber.getText().toString()),
+                                  name.getText().toString(), itemType.getText().toString(),
+                                  Integer.parseInt(itemNumber.getText().toString()),
+                                  itemSize.getText().toString());
+                    } catch (NumberFormatException nfe) {
+                        errorLog.setText("Invalid number format.");
+                    }
+                }
+            });
+        }
+        catch(Exception e) {
+            errorLog.setText("");
+            for(StackTraceElement line:e.getStackTrace()) {
+                errorLog.setText(errorLog.getText().toString()+line.toString()+'\n');
+            }
         }
     }
 
     /**
      * Stores the given data into the database
      *
-     * @return If storing worked
+     * @return If storing worked, true. Otherwise, false.
      */
     private boolean storeData(long barcodeNumber, String name, String itemType, int itemNumber,
                               String itemSize) {
--- app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/ScanActivity.java
+++ app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/ScanActivity.java
@@ -5,23 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ScanActivity extends AppCompatActivity {
 
     public static final String barcodeNumberName = "barcode_number";
 
+    private EditText manualInput;
+
+    private Button submit;
+
+    private TextView errorLog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
-        final TextView manualInput = (TextView) findViewById(R.id.manualInput);
-        Button submit = (Button) findViewById(R.id.submitButton);
+        manualInput = (EditText) findViewById(R.id.manualInput);
+        submit = (Button) findViewById(R.id.submitButton);
+        errorLog = (TextView) findViewById(R.id.errorLog);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToDataActivity(Long.parseLong(manualInput.getText().toString()));
+                try {
+                    goToDataActivity(Long.parseLong(manualInput.getText().toString()));
+                }
+                catch(NumberFormatException nfe) {
+                    goToDataActivity(-1L);
+                }
+                /*catch(Exception e) {
+                    errorLog.setText("");
+                    for(StackTraceElement line:e.getStackTrace()) {
+                        errorLog.setText(errorLog.getText().toString()+line.toString()+'\n');
+                    }
+                }*/
             }
         });
     }
