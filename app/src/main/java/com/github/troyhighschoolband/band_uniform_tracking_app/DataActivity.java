f958da98e91ba9cae48160ff9ac3ce43dd033a86
Jarred Allen <jarredallen73@gmail.com>
2017-07-29 21:46:43 -0700

    Some work on data page

--- app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/DataActivity.java
+++ app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/DataActivity.java
@@ -1,7 +1,15 @@
 package com.github.troyhighschoolband.band_uniform_tracking_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -9,17 +17,66 @@ public class DataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        Intent intent = getIntent();
+        long bn = intent.getLongExtra(ScanActivity.barcodeNumberName, -1);
+
+        TextView barcodeNumber = (TextView) findViewById(R.id.BarcodeNumberInput);
+        TextView name = (TextView) findViewById(R.id.ItemUserInput);
+        TextView itemType = (TextView) findViewById(R.id.ItemTypeInput);
+        TextView itemSize = (TextView) findViewById(R.id.ItemSizeInput);
+        TextView itemNumber = (TextView) findViewById(R.id.ItemNumberInput);
+        Button backButton = (Button) findViewById(R.id.backButton);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+
+        barcodeNumber.setText(String.valueOf(bn));
+        Data loaded = loadData(bn);
+        if (loaded.worked) {
+            name.setText(loaded.name);
+            itemType.setText(loaded.itemType);
+            itemSize.setText(loaded.itemSize);
+            itemNumber.setText(loaded.itemNumber);
+        }
     }
 
     /**
+     * Stores the given data into the database
      *
-     * @param barcodeNumber The number on the barcode
-     * @param type
-     * @param itemNumber
-     * @param size
      * @return If storing worked
      */
-    private boolean storeData(long barcodeNumber, ItemType type, int itemNumber, String size) {
+    private boolean storeData(long barcodeNumber, String name, String itemType, int itemNumber,
+                              String itemSize) {
         return false;
+    }
+
+    private Data loadData(long barcodeNumber) {
+        return new Data();
+    }
+
+
+    public class Data {
+        public boolean worked;
+        public long barcodeNumber;
+        public String name;
+        public String itemType;
+        public int itemNumber;
+        public String itemSize;
+
+        public Data() {
+            this(false, 0, null, null, 0, null);
+        }
+
+        public Data(long bn, String n, String it, int in, String is) {
+            this(true, bn, n, it, in, is);
+        }
+
+        public Data(boolean w, long bn, String n, String it, int in, String is) {
+            worked = w;
+            barcodeNumber = bn;
+            name = n;
+            itemType = it;
+            itemNumber = in;
+            itemSize = is;
+        }
     }
 }
--- app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/ScanActivity.java
+++ app/src/main/java/com/github/troyhighschoolband/band_uniform_tracking_app/ScanActivity.java
@@ -9,24 +9,26 @@ import android.widget.TextView;
 
 public class ScanActivity extends AppCompatActivity {
 
+    public static final String barcodeNumberName = "barcode_number";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
-        final TextView manual = (TextView) findViewById(R.id.manualInput);
+        final TextView manualInput = (TextView) findViewById(R.id.manualInput);
         Button submit = (Button) findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToDataActivity(Long.parseLong(manual.getText().toString()));
+                goToDataActivity(Long.parseLong(manualInput.getText().toString()));
             }
         });
     }
 
     private void goToDataActivity(long barcodeNumber) {
         Intent intent = new Intent(this, DataActivity.class);
-        intent.putExtra("barcode_number", barcodeNumber);
+        intent.putExtra(barcodeNumberName, barcodeNumber);
         startActivity(intent);
     }
 }
