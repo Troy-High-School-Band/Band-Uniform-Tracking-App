b9f9b1bb3cd1287229a72e980bfac3014d40ef08
Jarred Allen <jarredallen73@gmail.com>
2017-07-30 15:30:15 -0700

    Added a sample constants file
    This looks like the real one, but doesn't contain the actual data.

---
+++ app/src/main/java/com/github/troyhighschoolband/Constants.java
@@ -0,0 +1,10 @@
+package com.github.troyhighschoolband;
+
+/**
+ * Just a sample class, not the one with the actual stuff
+ */
+
+public final class Constants {
+    public static final String spreadsheetId = "Spreadsheet Id";
+    public static final String apiKey = "API KEY";
+}
